20affc7e38922ce3c62d9743b2670477d673cd99
DungNguyenManh <dung903698954@gmail.com>
2024-10-12 17:51:21 +0700

    5:51 12/10/2024

--- database/dbconnect.js
+++ database/dbconnect.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            dbName: "Library",
-            useNewUrlParser: true,       // Sử dụng parser mới của Mongoose
-            useUnifiedTopology: true      // Đảm bảo kết nối ổn định
+            dbName: "Library"
         });
         console.log("Connected to MongoDB");
     } catch (error) {
