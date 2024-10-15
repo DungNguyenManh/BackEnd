b78c2969433d0ca0909698b0208a052c76dd2d09
DungNguyenManh <dung903698954@gmail.com>
2024-10-15 20:59:27 +0700

    test

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = require('./app'); // import ứng dụng Express từ file app.js
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Example app listening at http://localhost:${PORT}`);
+// });
