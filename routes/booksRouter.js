df9bee96cee30397f030e5785b53671795217888
DungNguyenManh <dung903698954@gmail.com>
2024-10-25 22:12:36 +0700

    test

--- routes/booksRouter.js
+++ routes/booksRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bookRouter = express.Router();
-const multer = require('multer'); // Nếu không cần, bạn có thể bỏ import này
-const fs = require('fs');
+const multer = require('multer');
 
 const {
     getAllBooks,
@@ -11,12 +10,6 @@ const {
     deleteBook
 } = require('../controllers/bookController');
 const { authenticateToken, authorizeRole } = require('../middlewares/authMiddleware');
-
-// Nếu không cần thiết lập uploadDir, bạn có thể bỏ qua phần này
-const uploadDir = 'uploads'; // Có thể xóa nếu không sử dụng
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
 
 // Bạn có thể xóa phần cấu hình multer vì không cần upload nữa
 const upload = multer({
